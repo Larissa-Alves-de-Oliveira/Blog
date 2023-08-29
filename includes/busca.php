e4f1521b0e35d8971e392c6668b9d179591cffd9
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-08-29 11:15:06 -0300

    h

---
+++ includes/busca.php
@@ -0,0 +1,6 @@
+<form class="form-inline my-2 my-lg-O" action=''>
+    <input class="form-control mr-sm-2" type="search" name='busca' 
+    placeholder="Busca" arial-label="Busca">
+    <button class="btn btn-outline-success my-2 my-sm-O" 
+    type="submit">Buscar</button>
+</form>
