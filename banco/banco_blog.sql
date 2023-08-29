499fd0f97a14ce5202157af2f26bf4ff54981cc5
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-08-29 09:15:13 -0300

    banco

--- banco/banco_blog.sql
+++ banco/banco_blog.sql
@@ -24,3 +24,15 @@ CREATE TABLE post(
     KEY fk_post_usuario_idx (usuario_id),
     CONSTRAINT fk_post_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id)
 );
+
+create table avaliacao(
+	id int not null auto_increment,
+	nota int not null,
+	comentario varchar(255) not null,
+	usuario_id int not null,
+    post_id int not null,
+	data_criacao datetime not null default current_timestamp,
+	primary key (id),
+	constraint fk_avaliacao_usuario foreign key (usuario_id) references usuario (id),
+	constraint fk_avaliacao_post foreign key (post_id) references post (id)
+);
