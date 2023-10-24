f238be495460f7bc67046e612dd72356650331d5
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-10-24 10:30:05 -0300

    gf

--- includes/menu.php
+++ includes/menu.php
@@ -20,6 +20,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="usuarios.php">Usuários</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://bri.ifsp.edu.br/">IFSP</a>
+            </li>
             <?php endif; ?>
         </ul>
     </div>
